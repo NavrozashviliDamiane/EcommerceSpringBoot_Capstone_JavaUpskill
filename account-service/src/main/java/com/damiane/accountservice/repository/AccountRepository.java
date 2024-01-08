1d39ea970fc12d45e17b8bd97e70d4d10e17ba9f
NavrozashviliDamiane <navrozashvili1997@gmail.com>
2024-01-08 15:42:39 +0500

    Remove all files from repository

--- account-service/src/main/java/com/damiane/accountservice/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.damiane.accountservice.repository;
-
-import com.damiane.accountservice.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    Account findByUsername(String username);
-
-}
